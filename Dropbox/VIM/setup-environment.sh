6bd886ce994bf5577907ab3c0f4ce8e2d277161b
weijing <weijing.liu@nyu.edu>
2013-03-02 16:17:35 -0500

    init

---
+++ Dropbox/VIM/setup-environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+wget -O boost.tar.gz2 http://sourceforge.net/projects/boost/files/latest/download?source=files
+tar -xf boost.tar.gz2 -C .
+echo "boost installed"
+cp ~/Dropbox/Makfile .
+echo "Makefile ready"
+rm -rf ~/.vim ~/.vimrc
+ln -s ~/Dropbox/VIM/.vim ~/.vim
+ln -s ~/Dropbox/VIM/.vimrc ~/.vimrc
+echo "VIM setuped"
